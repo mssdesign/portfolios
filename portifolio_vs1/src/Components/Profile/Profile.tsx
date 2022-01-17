ef31260627fb13e686cf2c4aba0de7c6ccffaede
mssdesign <matheussilvasoares43@gmail.com>
2022-01-17 20:22:11 -0300

    port

--- portifolio_vs1/src/Components/Profile/Profile.tsx
+++ portifolio_vs1/src/Components/Profile/Profile.tsx
@@ -6,8 +6,8 @@ const Profile = () => {
         <div className="background_Home">
             <div className="profile_Picture">
                 <img className="avatar" src={require("../../Assets/profileMe.png")} alt="" />
+                <p className="legend">Olá, me chamo <strong className="legend-name">Matheus Soares</strong></p>
             </div>
-            <p className="legend">Olá, me chamo <strong className="legend-name">Matheus Soares</strong></p>
         </div>
     )
 }
