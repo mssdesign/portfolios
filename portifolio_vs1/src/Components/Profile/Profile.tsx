fa66107dcaf664655a8fd3c0046223a9c426fb57
mssdesign <matheussilvasoares43@gmail.com>
2022-01-17 20:34:50 -0300

    port

--- portifolio_vs1/src/Components/Profile/Profile.tsx
+++ portifolio_vs1/src/Components/Profile/Profile.tsx
@@ -1,15 +1,22 @@
-import React from "react"
+import React from 'react'
 import './Profile.css'
 
 const Profile = () => {
-    return (
-        <div className="background_Home">
-            <div className="profile_Picture">
-                <img className="avatar" src={require("../../Assets/profileMe.png")} alt="" />
-                <p className="legend">Olá, me chamo <strong className="legend-name">Matheus Soares</strong></p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="background_Home">
+      <div className="profile_Picture">
+        <img
+          className="avatar"
+          src={require('../../Assets/profileMe.png')}
+          alt=""
+        />
+        <p className="legend">
+          Olá, me chamo <strong className="legend-name">Matheus Soares</strong>
+        </p>
+      </div>
+      <div className="callToAction"></div>
+    </div>
+  )
 }
 
 export default Profile
