611792629e515b786938c2377062690aee776438
mssdesign <matheussilvasoares43@gmail.com>
2022-01-18 14:25:49 -0300

    port

--- portifolio_vs1/src/App.tsx
+++ portifolio_vs1/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './Components/Header/Header'
 import Profile from './Components/Profile/Profile'
+import UIDesign from './Components/UIDesign/UIDesign'
 
 function App() {
   return (
     <div className="background">
       <Header />
       <Profile />
+      <UIDesign />
     </div>
   )
 }
---
+++ portifolio_vs1/src/Components/UIDesign/UIDesign.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import './UIDesign.css'
+
+const UIDesign = () => {
+    return (
+        <div className="UIDesign_Page">
+            <h2>UI Designs</h2>
+            <div className="project">
+                <h3>Vzerolog</h3>
+            </div>
+        </div>
+    )
+}
+
+export default UIDesign
