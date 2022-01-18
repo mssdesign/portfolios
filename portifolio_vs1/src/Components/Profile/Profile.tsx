c4d74ef0c04ee825a05e86787cbc9ed79315798b
mssdesign <matheussilvasoares43@gmail.com>
2022-01-17 22:07:13 -0300

    port

--- portifolio_vs1/src/Components/Profile/Profile.tsx
+++ portifolio_vs1/src/Components/Profile/Profile.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Avatar from '../../Assets/down.svg'
 import './Profile.css'
 
 const Profile = () => {
@@ -14,7 +15,10 @@ const Profile = () => {
           Olá, me chamo <strong className="legend-name">Matheus Soares</strong>
         </p>
       </div>
-      <div className="callToAction"></div>
+      <div className="callToAction">
+        <p>Ver projetos</p>
+        <img src={Avatar} />
+      </div>
     </div>
   )
 }
