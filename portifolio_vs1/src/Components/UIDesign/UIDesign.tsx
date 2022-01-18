c50a6570ee852f54b30dd81fae8166dd7b587cb7
mssdesign <matheussilvasoares43@gmail.com>
2022-01-18 17:56:26 -0300

    react

--- portifolio_vs1/src/Components/UIDesign/UIDesign.tsx
+++ portifolio_vs1/src/Components/UIDesign/UIDesign.tsx
@@ -1,27 +1,31 @@
-import React from "react"
+import React from 'react'
 import './UIDesign.css'
 import Vzero from '../../Assets/VzeroMock.png'
 import MAR from '../../Assets/MARMock.png'
+import PersonaFit from '../../Assets/PersonaFit.png'
 
 const UIDesign = () => {
-    return (
-        <div className="UIDesign_Page">
-            <div className="UIDesign_Page_main">
-                <h2>UI Designs</h2>
-                <div className="Mockups">
-                    <div className="Mockup_container">
-                        <img src={Vzero} className="mockup" />
-                    </div>
-                    <div className="Mockup_container">
-                        <img src={MAR} className="mockup" />
-                    </div>
-                    <div className="Mockup_container">
-                        <img src={Vzero} className="mockup" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="UIDesign_Page">
+      <div className="UIDesign_Page_main">
+        <h2>UI Designs</h2>
+        <div className="Mockups">
+          <div className="Mockup_container">
+            <h4>Vzerolog</h4>
+            <img src={Vzero} className="mockup" />
+          </div>
+          <div className="Mockup_container">
+            <h4>MAR</h4>
+            <img src={MAR} className="mockup" />
+          </div>
+          <div className="Mockup_container">
+            <h4>PersonaFit</h4>
+            <img src={PersonaFit} className="mockup" />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default UIDesign
