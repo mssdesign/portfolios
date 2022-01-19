6b6b1aa92befe59d387c03aa8e6f1ab512c533b2
mssdesign <matheussilvasoares43@gmail.com>
2022-01-19 18:20:48 -0300

    port

--- portifolio_vs1/src/Components/Websites/Websites.tsx
+++ portifolio_vs1/src/Components/Websites/Websites.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import './Websites.css'
+import AnimatedLogin from '../../Assets/WebSitesPreview/Animated_login.png'
 
 const Websites = () => {
     return (
@@ -7,15 +8,42 @@ const Websites = () => {
             <div className="Websites_Page_main">
                 <h2>Webpages</h2>
                 <div className="Websites">
-                    <div className="Website_card"></div>
-                    <div className="Website_card"></div>
-                    <div className="Website_card"></div>
-                    <div className="Website_card"></div>
-                    <div className="Website_card"></div>
-                    <div className="Website_card"></div>
-                    <div className="Website_card"></div>
-                    <div className="Website_card"></div>
-                    <div className="Website_card"></div>
+                    <div className="Website_card">
+                        <img src={AnimatedLogin} alt="" />
+                        <p>Login Page</p>
+                    </div>
+                    <div className="Website_card">
+                        <img src={AnimatedLogin} alt="" />
+                        <p>Login Page</p>
+                    </div>
+                    <div className="Website_card">
+                        <img src={AnimatedLogin} alt="" />
+                        <p>Login Page</p>
+                    </div>
+                    <div className="Website_card">
+                        <img src={AnimatedLogin} alt="" />
+                        <p>Login Page</p>
+                    </div>
+                    <div className="Website_card">
+                        <img src={AnimatedLogin} alt="" />
+                        <p>Login Page</p>
+                    </div>
+                    <div className="Website_card">
+                        <img src={AnimatedLogin} alt="" />
+                        <p>Login Page</p>
+                    </div>
+                    <div className="Website_card">
+                        <img src={AnimatedLogin} alt="" />
+                        <p>Login Page</p>
+                    </div>
+                    <div className="Website_card">
+                        <img src={AnimatedLogin} alt="" />
+                        <p>Login Page</p>
+                    </div>
+                    <div className="Website_card">
+                        <img src={AnimatedLogin} alt="" />
+                        <p>Login Page</p>
+                    </div>
                 </div>
             </div>
         </div>
