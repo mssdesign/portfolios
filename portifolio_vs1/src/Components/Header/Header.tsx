8ec5905a790986c5da8b9211dc17fdaa4ed7363a
mssdesign <matheussilvasoares43@gmail.com>
2022-01-21 08:05:54 -0300

    port

--- portifolio_vs1/src/Components/Header/Header.tsx
+++ portifolio_vs1/src/Components/Header/Header.tsx
@@ -4,13 +4,13 @@ import './Header.css'
 const Header = () => {
   return (
     <div className="navbar__menu">
-      <a href="/" className="navbar__menu--links active">
+      <a href="/" className="navbar__menu--links">
         Página Inicial
       </a>
       <a href="/" className="navbar__menu--links">
         UX/UI Designs
       </a>
-      <a href="/" className="navbar__menu--links">
+      <a href="/" className="navbar__menu--links active">
         Webpages
       </a>
       <a href="/" className="navbar__menu--links" id="button">
